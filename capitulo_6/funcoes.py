0d9051a236ca507d1cdf9ff3fb4ac689f9314a3d
PeterSilva84 <peterdasilva84@gmail.com>
2024-07-01 22:39:47 -0300

    second commit

---
+++ capitulo_6/funcoes.py
@@ -0,0 +1,8 @@
+#Escopo Global
+def minha_func():
+    print('Minha primeira função!')
+
+
+minha_func()
+
+print(minha_func)
---
+++ capitulo_6/tipo_none.py
@@ -0,0 +1,12 @@
+
+a = 4
+b = 3
+
+def soma():
+    a = int(input('Entre com a: '))
+    b = int(input('Entre com b: '))
+    return a + b
+
+res = soma()
+
+print(res)
