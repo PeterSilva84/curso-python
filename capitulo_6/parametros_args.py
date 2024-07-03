3c172a93ebb99392d7e31f46ee55a4441bc7666d
PeterSilva84 <peterdasilva84@gmail.com>
2024-07-02 22:17:38 -0300

    second commit

---
+++ capitulo_6/parametros_args.py
@@ -0,0 +1,12 @@
+def soma(a: int | None = 0, b: int | None = 0) -> int:
+    '''
+       Essa função serve para somar 2 argumentos
+    :param a:
+    :param b:
+    :return:
+    '''
+
+    return a + b
+
+
+soma()
